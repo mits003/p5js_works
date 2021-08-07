167c76f41c03e8cfcbcd3173376efd76e102765f
mits003 <miyake@mierune.co.jp>
2021-08-07 23:44:58 +0900

    add works

--- works/20210807_2/js/particle.js
+++ works/20210807_2/js/particle.js
@@ -1,13 +1,10 @@
 function Particle(left, right, top, bottom) {
-    // this.pos = createVector(random(width), random(height));
     this.pos = createVector(random(left, right), random(top, bottom));
-    // this.pos = createVector(right, top);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
-    this.maxspeed = 4;
-    this.prevPos = this.pos.copy();
+    this.maxspeed = 2;
 
-    this.alpha_v = 30;
+    this.prevPos = this.pos.copy();
 
     this.left_x = int(-width * 0);
     this.right_x = int(width * 1);
@@ -15,7 +12,6 @@ function Particle(left, right, top, bottom) {
     this.bottom_y = int(height * 1);
 
     this.update = function () {
-        // this.vel.add(this.center);
         this.vel.add(this.acc);
         this.vel.limit(this.maxspeed);
         this.pos.add(this.vel);
@@ -35,31 +31,30 @@ function Particle(left, right, top, bottom) {
     };
 
     this.show = function () {
-        stroke(c.r, c.g, c.b, this.alpha_v)
-        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+        strokeWeight(3);
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y)
         this.updatePrev();
     };
 
-    this.updatePrev = function(){
+    this.updatePrev = function () {
         this.prevPos.x = this.pos.x;
         this.prevPos.y = this.pos.y;
-
-    }
+    };
 
     this.edges = function () {
         if (this.pos.x > this.right_x) {
             this.pos.x = this.left_x;
             this.updatePrev();
         }
-        if (this.pos.x < this.left_x){
+        if (this.pos.x < this.left_x) {
             this.pos.x = this.right_x;
             this.updatePrev();
         }
-        if (this.pos.y > this.bottom_y){
+        if (this.pos.y > this.bottom_y) {
             this.pos.y = this.top_y;
             this.updatePrev();
         }
-        if (this.pos.y < this.top_y){
+        if (this.pos.y < this.top_y) {
             this.pos.y = this.bottom_y;
             this.updatePrev();
         }
---
+++ works/20210807_2/js/sketch.js
@@ -0,0 +1,74 @@
+// カラーパレット
+
+let colors1 = ['#001219','#005f73','#0a9396','#94d2bd','#e9d8a6','#ee9b00','#ca6702','#bb3e03','#ae2012','#9b2226']
+let colors2 = ['#f94144','#f3722c','#f8961e','#f9844a','#f9c74f','#90be6d','#43aa8b','#4d908e','#577590','#277da1','#f94144','#f3722c','#f8961e','#f9844a','#f9c74f','#90be6d','#43aa8b','#4d908e','#577590','#277da1','#f94144','#f3722c','#f8961e','#f9844a','#f9c74f','#90be6d','#43aa8b','#4d908e','#577590','#277da1']
+let colors3 = ['#001d3d','#002952','#002c57','#002f5c','#124d85','#246bae','#47a6ff','#ffc300','#ffcf10','#ffda1f','#001d3d','#002952','#002c57','#002f5c','#124d85','#246bae','#47a6ff','#ffc300','#ffcf10','#ffda1f','#001d3d','#002952','#002c57','#002f5c','#124d85','#246bae','#47a6ff','#ffc300','#ffcf10','#ffda1f',]
+
+
+let colorPalettes = [colors1, colors2, colors3]
+
+let inc = 0.03;
+let scl = 10;
+let cols, rows;
+
+let zoff = 0;
+
+let particles_num = 50;
+let particles  = [];
+
+let flowfield;
+
+let fr = 50;
+
+function setup() {
+    // setTimeout('saveCanvas(canvas, "myCanvas", "png")', 1000);
+    frameRate(fr)
+    let canvas = createCanvas(600, 600);
+
+    let left_x = int(-width * 0);
+    let right_x = int(width * 1);
+    let top_y = int(-height * 0);
+    let bottom_y = int(height * 1);
+
+    cols = floor((right_x - left_x) / scl);
+    rows = floor((bottom_y - top_y) / scl);
+
+    flowfield = new Array(cols * rows);
+
+    for (let i = 0; i < particles_num; i++) {
+        particles[i] = new Particle(left_x, right_x, top_y, bottom_y);
+    }
+    background(255, 245, 194);
+}
+
+function draw() {
+    let yoff = 0;
+    for (let y = 0; y < rows; y++) {
+        let xoff = 0;
+        for (let x = 0; x < cols; x++) {
+            let index = x + y * cols;
+            let angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+            let v = p5.Vector.fromAngle(angle);
+            v.setMag(0.1)
+            flowfield[index] = v;
+            xoff += inc;
+            // stroke(0, 50);
+            // push();
+            // translate(x * scl, y * scl);
+            // rotate(v.heading());
+            // strokeWeight(1)
+            // line(0, 0, scl, 0);
+            // pop()
+        }
+        yoff += inc;
+        zoff += 0.0001;
+    }
+
+    for (let i = 0; i < colorPalettes[1].length; i++) {
+        stroke(colorPalettes[1][i])
+        particles[i].follow(flowfield);
+        particles[i].update();
+        particles[i].edges();
+        particles[i].show();
+    }
+}
