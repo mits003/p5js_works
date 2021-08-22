d0c20d560962c035c6a2c2ad157d08f369d178cd
mits003 <miyake@mierune.co.jp>
2021-08-22 17:16:35 +0900

    add works

--- works/20210821/js/sketch.js
+++ works/20210821/js/sketch.js
@@ -1,7 +1,7 @@
-let cp1 = ['#003049','#d62828','#f77f00','#fcbf49','#eae2b7']
-let cp2 = ['#d6d6d6','#ffee32','#ffd100','#5f2f2f','#232323']
-let cp3 = ['#5f0f40','#9a031e','#fb8b24','#e36414','#0f4c5c']
-let cp4 = ['#0d3b66','#faf0ca','#f4d35e','#ee964b','#f95738']
+let cp1 = ['#003049aa','#d62828aa','#f77f00aa','#fcbf49aa','#eae2b7aa']
+let cp2 = ['#d6d6d6aa','#ffee32aa','#ffd100aa','#5f2f2faa','#232323aa']
+let cp3 = ['#5f0f40aa','#9a031eaa','#fb8b24aa','#e36414aa','#0f4c5caa']
+let cp4 = ['#0d3b66aa','#faf0caaa','#f4d35eaa','#ee964baa','#f95738aa']
 let colorPalettes = [cp1, cp2, cp3, cp4]
 
 let BASE_S = 20;
@@ -14,39 +14,58 @@ function setup(){
     background(BASE_H, BASE_S, BASE_B);
     texturize(BASE_H, 30000);
 
-    let circles = random(4, 8);
     let circlesCp = random(colorPalettes)
-    let cells = 20;
-    let offset = width / 15;
-    let margin = 0;
-    let w = (width - offset * 2 - margin * (cells - 1)) / cells;
-    let h = (height - offset * 2 - margin * (cells - 1)) / cells;
+    let fcells = 20;
+    let foffset = width / 15;
+    let fmargin = 0;
+    let fw = (width - foffset * 2 - fmargin * (fcells - 1)) / fcells;
+    let fh = (height - foffset * 2 - fmargin * (fcells - 1)) / fcells;
+    let bcells = floor(fcells * 0.25);
+    let boffset = foffset;
+    let bmargin = 0;
+    let bw = (width - boffset * 2 - bmargin * (bcells - 1)) / bcells;
+    let bh = (height - boffset * 2 - bmargin * (bcells - 1)) / bcells;
 
-    for (let i = 0; i < circles; i++){
-        noStroke();
-        fill(random(circlesCp));
-        circle(random(offset*2, width-offset*2), random(offset*2, height-offset*2), random(40, offset*2));
+
+    noStroke()
+    for (let j = 0; j < bcells; j++){
+        for (let i = 0; i < bcells; i++){
+            let x = boffset + (i * (bw + bmargin));
+            let y = boffset + (j * (bh + bmargin));
+
+            let cx = x + bw / 2;
+            let cy = y + bh / 2;
+
+            let choice = random(5);
+            if (choice < 1) {
+                push();
+                translate(cx, cy);
+                fill(random(circlesCp))
+                circle(random(-bw/2, bw/2), random(-bh/2, bh/2), random(40, foffset*2));
+                pop()
+            }
+        }
     }
 
     stroke(BASE_H + random([-20, 20]), 50, 50, 1.0)
-    for (let j = 0; j < cells; j++){
-        for (let i = 0; i < cells; i++){
-                let x = offset + i * (w + margin);
-                let y = offset + j * (h + margin);
+    for (let j = 0; j < fcells; j++){
+        for (let i = 0; i < fcells; i++){
+                let x = foffset + i * (fw + fmargin);
+                let y = foffset + j * (fh + fmargin);
 
-                let cx = x + w / 2;
-                let cy = y + h / 2;
+                let cx = x + fw / 2;
+                let cy = y + fh / 2;
 
                 push();
                 translate(cx, cy);
 
                 let theta = 90 * random([0, 1]);
                 if (theta === 0) {
-                    line(0, -h/2, w/2, 0);
-                    line(-w/2, 0, 0, h/2);
+                    line(0, -fh/2, fw/2, 0);
+                    line(-fw/2, 0, 0, fh/2);
                 } else if (theta === 90) {
-                    line(-w/2, 0, 0, -h/2);
-                    line(0, h/2, h/2, 0);
+                    line(-fw/2, 0, 0, -fh/2);
+                    line(0, fh/2, fh/2, 0);
                 }
 
                 pop();
@@ -73,6 +92,6 @@ function texturize(base_h, density){
 }
 
 function mouseClicked(){
-    // saveCanvas(canvas, 'myCanvas', 'png');
-    window.location.reload();
+    // saveCanvas(canvas, 'myCanvasaa', 'png');
+   window.location.reload();
 }
--- works/20210822_2/js/sketch.js
+++ works/20210822_2/js/sketch.js
@@ -1,8 +1,7 @@
-let cp1 = ['#486c99aa','#b0a0baaa','#c38391aa'];
-let cp2 = ['#00838faa','#d1e8f0aa','#d86d4aa6'];
-let cp3 = ['#5f0f40aa','#9a031eaa','#0f4c5caa'];
-let cp4 = ['#023047aa','#ffb703aa','#fb8500aa'];
-
+let cp1 = ['#588b8baa','#ffffffaa','#f4d35eaa','#f28f3baa','#c8553daa'];
+let cp2 = ['#283d3baa','#197278aa','#edddd4aa','#c4453aa6','#772e25aa'];
+let cp3 = ['#f4f1deaa','#e07a5faa','#3d405baa','#81b29aaa','#f2cc8faa'];
+let cp4 = ['#e6394aa6','#f1faeeaa','#a8dadcaa','#457b9daa','#1d3557aa'];
 let colorPalettes = [cp1, cp2, cp3, cp4]
 
 let BASE_S = 20;
@@ -10,20 +9,18 @@ let BASE_B = 70;
 
 function setup(){
     createCanvas(600, 600);
-    rectMode(CENTER);
-    angleMode(DEGREES)
     colorMode(HSB, 360, 100, 100, 1.0);
     let BASE_H = random(0, 360);
     background(BASE_H, BASE_S, BASE_B);
     texturize(BASE_H, 30000);
-    
-    let cp = random(colorPalettes);
-    let fcells = 10;
+
+    let circlesCp = random(colorPalettes)
+    let fcells = 20;
     let foffset = width / 15;
     let fmargin = 0;
     let fw = (width - foffset * 2 - fmargin * (fcells - 1)) / fcells;
     let fh = (height - foffset * 2 - fmargin * (fcells - 1)) / fcells;
-    let bcells = floor(fcells * 0.65);
+    let bcells = floor(fcells * 0.25);
     let boffset = foffset;
     let bmargin = 0;
     let bw = (width - boffset * 2 - bmargin * (bcells - 1)) / bcells;
@@ -37,23 +34,20 @@ function setup(){
 
             let cx = x + bw / 2;
             let cy = y + bh / 2;
-            let d = random(bw * 0.2, fw * 1.2);
-            let a = random(-2, 2)
 
             let choice = random(5);
             if (choice < 1) {
                 push();
                 translate(cx, cy);
-                rotate(a);
-                fill(random(cp))
-                rect(random(-bw/2, bw/2), random(-bh/2, bh/2), d);
+                fill(random(circlesCp))
+                circle(random(-bw/2, bw/2), random(-bh/2, bh/2), random(40, foffset*2));
                 pop()
             }
         }
     }
 
-    stroke(80);
-    noFill()
+    stroke(BASE_H + random([-20, 20]), 50, 50, 1.0);
+    noFill();
     for (let j = 0; j < fcells; j++){
         for (let i = 0; i < fcells; i++){
             let x = foffset + i * (fw + fmargin);
@@ -61,16 +55,23 @@ function setup(){
 
             let cx = x + fw / 2;
             let cy = y + fh / 2;
-            let d = random(fw * 0.4, fw * 1.2);
 
             push();
             translate(cx, cy);
-            rect(0, 0, d);
-            pop()
+            let theta = HALF_PI * int(random(4));
+            rotate(theta);
+            let choice = int(random(1));
+            if (choice === 0){
+                arc(-fw/2, -fh/2, fw, fh, 0, HALF_PI);
+                arc(fw/2, fh/2, fw, fh, PI, HALF_PI * 3);
+            } else {
+                arc(-fw/2, fh/2, fw, fh, HALF_PI, 0);
+                arc(fw/2, -fh/2, fw, fh, HALF_PI, PI);
+            }
+            pop();
         }
     }
 }
-
 
 function texturize(base_h, density){
     for(let i = 0; i < density; i++) {
@@ -81,7 +82,7 @@ function texturize(base_h, density){
 
         let x1 = random(width);
         let y1 = random(height);
-        let theta = random(360);
+        let theta = random(TWO_PI);
         let segmentLength = random(2, 7);
         let x2 = cos(theta) * segmentLength + x1;
         let y2 = sin(theta) * segmentLength + y1;
@@ -91,6 +92,6 @@ function texturize(base_h, density){
 }
 
 function mouseClicked(){
-    // saveCanvas(canvas, 'myCanvas', 'png');
+    // saveCanvas(canvas, 'myCanvasaa', 'png');
     window.location.reload();
 }
