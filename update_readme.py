f33ca12942ec9d21a1a0996b6b739a9679dd5cd3
mitsuha miyake <3shinyleaves@gmail.com>
2021-11-15 02:49:58 +0900

    update ga

--- update_readme.py
+++ update_readme.py
@@ -4,7 +4,7 @@ import glob
 def get_filenames():
     works_dict = {}
     works_dirs = glob.glob("./works/*", recursive=False)
-    works_dirs.sort()
+    works_dirs.sort(reverse=True)
 
     for dir in works_dirs:
         work = dir.split("/")[-1]
@@ -18,7 +18,6 @@ def write_readme(works_dict):
     path_w = 'README.md'
     with open(path_w, mode='w', newline='\n') as f:
         for work, imgs in works_dict.items():
-            # f.write(work + "\n")
             f.write(f'<h2><a href="/works/{work}/README.md">{work}</a></h2>\n')
             for i in imgs:
                 f.write(f'<img src="{i}" width="30%">\n')
