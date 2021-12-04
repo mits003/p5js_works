eb30133e2b7d5734a12e50dd682698ec9423aaf7
mitsuha miyake <3shinyleaves@gmail.com>
2021-12-04 13:37:09 +0900

    add images and minor fix

--- works/20210422/js/sketch.js
+++ works/20210422/js/sketch.js
@@ -7,11 +7,11 @@ function setup() {
 
     colorMode(HSB, 360, 100, 100, 100);
     // 背景色を指定
-    background('#206a5d');
+    background(random(colors));
 
-    stroke('#ffcc29');
+    stroke(random(colors));
     strokeWeight(5);
-    noFill()
+    noFill();
 
     let lines = 3;
     let steps = 16;
@@ -23,15 +23,10 @@ function setup() {
         push();
         for (let s = 0; s < steps; s++) {
             let theta = map(noise(l,s*0.1), 0, 1, -0.5, 0.5) * PI;
-            // let theta = noise(1, 1) * PI;
-            // let theta = noise(s * 0.1, l) * PI;
-            console.log(theta)
-            translate(0, w)
-            rotate(theta)
-            ellipse(0, 0, w)
+            translate(0, w);
+            rotate(theta);
+            ellipse(0, 0, w);
         }
-        pop()
+        pop();
     }
-
-    // setTimeout('saveCanvas(canvas, "myCanvas", "png")', 1000);
 }
