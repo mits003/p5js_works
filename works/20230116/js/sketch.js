948d873a9816f472c23815c39b0c9b5a026cd1b8
Mitsuha Miyake <mitsuha@MitsuhanoMacBook-Air.local>
2023-01-16 21:55:55 +0900

    update works

--- works/20230116/js/sketch.js
+++ works/20230116/js/sketch.js
@@ -4,18 +4,21 @@ let colors = ["#023e8a","#0077b6","#00b4d8","#90e0ef", "#CAF0F8"];
 function setup() {
     let canvas = createCanvas(600, 600);
 
+    angleMode(DEGREES)
+
     background('#03045E');
     ni1 = random(0.1, 0.5)
     nj1 = random(0.1, 0.5)
 
-    for (let j = 4; j < 6; j++){
+    for (let j = 3; j < 6; j++){
         let radiusFirst = width/12*j;
+        let radiusBase = random(10, 40);
+        let puffNum = int(random(3, 8))
         for(let i = 0; i < 360; i +=3){
-            let angle = i / 180 * PI;
-            let radiusBase = 30;
-            let radiusSecond = radiusBase * sin(angle*8) + radiusBase;
+            let angle = i;
+            let radiusSecond = radiusBase * sin(angle*puffNum) + radiusBase;
             let x = radiusFirst * cos(angle) + width/2;
-            let y = radiusFirst * sin(angle-15) + height/2;
+            let y = radiusFirst * sin(angle) + height/2;
     
             noFill();
             let n = floor(map(noise(i*ni1, j*nj1), 0, 1, 0, colors.length));
