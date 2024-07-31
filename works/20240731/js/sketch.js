52c335d0dc6635caf5f0813a5b5b0a9eef17801d
Mitsuha Miyake <mitsuha@MitsuhanoMacBook-Air.local>
2024-07-31 20:20:53 +0900

    add an artwork 20240731

---
+++ works/20240731/js/sketch.js
@@ -0,0 +1,100 @@
+let seed = 2000;
+let rSeed;
+
+let start = 100;
+
+let rep = 100;
+
+function setup() {
+  // キャンバスサイズの設定
+  let canvas = createCanvas(600, 600);
+
+  angleMode(DEGREES);
+
+  noiseDetail(2, 1);
+
+  rSeed = int(random(seed));
+  console.log(rSeed);
+  randomSeed(rSeed);
+  noiseSeed(rSeed);
+
+  background(30);
+
+  translate(width / 2, height / 2);
+
+  let space = 0.1;
+  for (let j = 0; j < rep; j++) {
+    for (let i = 0; i < 360; i += space) {
+      let xoff = map(cos(i) + j + 1, -1, 1, 0, 3);
+      let yoff = map(sin(i) + j, -1, 1, 0, 3);
+
+      let n = noise(xoff + start, yoff + start);
+      let h = map(n, 0, 1, -300, 300);
+
+      rotate(space);
+      stroke(255, 255, 255, 30);
+      point(h, 1);
+    }
+  }
+
+  let c = random();
+
+  for (let j = 0; j < rep; j++) {
+    for (let i = 0; i < 360; i += space) {
+      let xoff = map(cos(i) + j, -1, 1, 0, 3);
+      let yoff = map(sin(i) + j, -1, 1, 0, 3);
+
+      let n = noise(xoff + start, yoff + start);
+      let h = map(n, 0, 1, -300, 300);
+
+      let r, g, b;
+      if (c > 5 / 6) {
+        // blue and purple base
+        r = map(sin(i), -1, 1, 100, 200);
+        g = map(h, -150, 150, 0, 150);
+        b = map(n, 0, 1, 150, 255);
+      } else if (c > 4 / 6) {
+        // pink and yello base
+        b = map(sin(i), -1, 1, 100, 200);
+        g = map(h, -150, 150, 0, 150);
+        r = map(n, 0, 1, 150, 255);
+      } else if (c > 3 / 6) {
+        // yellow and red base
+        g = map(sin(i), -1, 1, 100, 200);
+        b = map(h, -150, 150, 0, 150);
+        r = map(n, 0, 1, 150, 255);
+      } else if (c > 2 / 6) {
+        // yellow and red base
+        g = map(sin(i), -1, 1, 100, 200);
+        r = map(h, -150, 150, 0, 150);
+        b = map(n, 0, 1, 150, 255);
+      } else if (1 / 6) {
+        // yellow and red base
+        r = map(sin(i), -1, 1, 100, 200);
+        b = map(h, -150, 150, 0, 150);
+        g = map(n, 0, 1, 150, 255);
+      } else {
+        // yellow and red base
+        r = map(sin(i), -1, 1, 100, 200);
+        b = map(h, -150, 150, 0, 150);
+        g = map(n, 0, 1, 150, 255);
+      }
+      rotate(space);
+      noStroke();
+      fill(r, g, b, 30);
+      rect(0, 0, h, 1);
+    }
+  }
+}
+
+function mouseClicked() {
+  window.location.reload();
+}
+
+function keyPressed() {
+  if (key == "s") {
+    var scripts = document.getElementsByTagName("sketch").src;
+    console.log(scripts);
+    saveCanvas(canvas, "image_" + rSeed, "png");
+  }
+}
